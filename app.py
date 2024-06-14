0a68b9de82f0bedef0e7a4f16e7b99dbfb26069c
Karan Khatavkar <karank1072@gmail.com>
2024-06-14 14:09:50 +0530

    Update for prod

--- app.py
+++ app.py
@@ -16,12 +16,11 @@ def predict():
     email = request.form.get('email')
     
     # Prdict Email 
-    print(email)
     prediction = model.predict(vectorizer.transform([email]))
     prediction = 1 if prediction == 1 else -1
 
     return render_template('index.html', response=prediction)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run()
